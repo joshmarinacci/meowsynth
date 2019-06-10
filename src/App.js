9928f950796f3f2aef7e7f489bd03b975d1dbf7b
joshmarinacci <joshua@marinacci.org>
2019-06-10 13:32:45 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+const sequences = [
+    {
+        position: {
+            x:10,
+            y:10,
+        },
+        pitches:['A1','B1','C1','D1'],
+        instrument:{
+            name:'synth1'
+        },
+    },
+    {
+        position:{
+            x:20,
+            y:200,
+        },
+        pitches:['D1'],
+        instrument:{
+            name:'drum'
+        }
+    }
+]
+
+class SequenceView extends Component {
+    render() {
+        return <div className="sequence-view" style={{
+            position:'absolute',
+            left:this.props.sequence.position.x,
+            top:this.props.sequence.position.y,
+        }}>
+            {
+                this.props.sequence.pitches.map((i)=>{
+                    return <SequenceRow key={i}/>
+                })
+            }
+        </div>
+    }
+}
+
+class SequenceRow extends Component {
+    render() {
+        const beats = []
+        for(let i=0; i<16; i++) {
+            beats.push(<SequenceNote key={i} beat={i}/>)
+        }
+        return <div className="sequence-row">{beats}</div>
+    }
+}
+
+class SequenceNote extends Component {
+    render() {
+        return <button className="note"></button>
+    }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout-canvas">
+        {sequences.map((seq,i)=>{
+            return <SequenceView sequence={seq} key={i}/>
+        })}
     </div>
   );
 }
