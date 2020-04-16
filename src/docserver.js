d433b0497d963d184811aec1218b93a1578759f6
josh <joshua@marinacci.org>
2020-04-16 08:24:34 -0700

    actually log in and out

--- src/docserver.js
+++ src/docserver.js
@@ -1,17 +1,49 @@
-import React, {Component, useState, useContext} from 'react'
+import React, {Component, useState, useContext, useEffect} from 'react'
+export const LOGIN = "LOGIN"
 
 export class DocServerAPI {
     constructor(url) {
         this.url = url
+        this.listeners = {}
+    }
+    on(type, cb) {
+        if (!this.listeners[type]) this.listeners[type] = []
+        this.listeners[type].push(cb)
+    }
+
+    off(type, cb) {
+        if (this.listeners[type])
+            this.listeners[type] = this.listeners[type].filter(c => c !== cb)
+    }
+    fire(type, payload) {
+        if (!this.listeners[type]) this.listeners[type] = []
+        this.listeners[type].forEach(cb => cb(payload))
     }
     isLoggedIn() {
+        if (localStorage.getItem('access-token')) return true
         return false
     }
     startLogin() {
         console.log("starting to do the login")
+        let auth_url = `${this.url}auth/github/`
+        this.win = window.open(auth_url, '_blank')
+        window.addEventListener('message', this.authCallback)
+        if (this.win) this.win.focus()
+    }
+    startLogout() {
+        localStorage.clear()
+        this.fire(LOGIN, {})
+    }
+    authCallback = (msg) => {
+        console.log("got the auth callback", msg.data)
+        localStorage.setItem('access-token', msg.data.payload.accessToken)
+        localStorage.setItem('username', msg.data.payload.username)
+        this.win.close()
+        window.removeEventListener('message', this.authCallback)
+        this.fire(LOGIN, {})
     }
     getUsername() {
-        return "dummy username"
+        return localStorage.getItem('username')
     }
     listDocs(filter) {
 
@@ -28,8 +60,20 @@ export const DocServerContext = React.createContext()
 
 export const LoginButton = ({})=> {
     const ds = useContext(DocServerContext)
+    const [li, setLi] = useState(ds.isLoggedIn())
+    useEffect(()=>{
+        let cb = () => {
+            console.log('login changed')
+            setLi(ds.isLoggedIn())
+        }
+        ds.on(LOGIN,cb)
+        return () => {
+            console.log('rebuilding')
+            ds.off(LOGIN,cb)
+        }
+    },li)
     if(ds.isLoggedIn()) {
-        return <button>{ds.getUsername()}</button>
+        return <button onClick={()=>ds.startLogout()}>{ds.getUsername()}</button>
     }
     return <button onClick={()=>{
         ds.startLogin()
